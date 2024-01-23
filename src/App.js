d959d105931d88bfeb845e55ec9082c97fa40c3d
Tushar <tusharchemate07@gmail.com>
2024-01-23 23:25:23 +0530

    added balance comp in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Balance from './Balance';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </div>
   );
 }
