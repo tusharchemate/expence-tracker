6706f8cfe875f0082ad614cd02684bc7812e98ac
Tushar <tusharchemate07@gmail.com>
2024-01-23 23:31:00 +0530

    added income expense in the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Balance from './Balance';
+import IncomeExpenses from './IncomeExpenses';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
       </div>
     </div>
   );
