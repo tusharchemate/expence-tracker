a9db636b8be51290ab0a068ac494a71ab045e089
Tushar <tusharchemate07@gmail.com>
2024-01-23 23:54:40 +0530

    added global provider in app

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import IncomeExpenses from './IncomeExpenses';
 import TransactionList from './TransactionList';
 import AddTransaction from './AddTransaction';
 
+import { GlobalProvider } from './ context/GlobalState';
+
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -16,7 +18,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
