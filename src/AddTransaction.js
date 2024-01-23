23cf31172dbc4ce7a259363de86aa9de9d559e0d
Tushar <tusharchemate07@gmail.com>
2024-01-23 23:41:49 +0530

    added state in addtransaction comp

--- src/AddTransaction.js
+++ src/AddTransaction.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
   return (
     <>
       <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter Text ..." />
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            type="text"
+            placeholder="Enter Text ..."
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" placeholder="Enter amount ..." />
+          <input
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            type="number"
+            placeholder="Enter amount ..."
+          />
         </div>
 
         <button className="btn">Add transaction</button>
