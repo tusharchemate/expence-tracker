18ecc7f1ef8f65118cd17d646d2d9f3178d672cb
Tushar <tusharchemate07@gmail.com>
2024-01-27 13:16:34 +0530

    added income - expense in incomeexpens component

--- src/IncomeExpenses.js
+++ src/IncomeExpenses.js
@@ -3,20 +3,34 @@ import { GlobalContext } from './ context/GlobalState';
 
 const IncomeExpenses = () => {
   const { transactions } = useContext(GlobalContext);
+  const amount = transactions.map((item) => item.amount);
+
+  //calculating income
+
+  const income = amount
+    .filter((item) => item > 0)
+    .reduce((acc, curr) => (acc += curr), 0)
+    .toFixed(2);
+
+  // calculating expense
+  const expense = amount
+    .filter((item) => item < 0)
+    .reduce((acc, curr) => (acc += curr), 0)
+    .toFixed(2);
 
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
         <p id="money-plus" className="money plus">
-          + $0.00
+          + $ {income}
         </p>
       </div>
 
       <div>
         <h4>Expense</h4>
         <p id="money-minus" className="money minus">
-          - $0.00
+          - $ {expense}
         </p>
       </div>
     </div>
